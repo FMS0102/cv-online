d56e6c90957adaf0d9b98ccdaa32d236a709ad27
Felipe M. Simões <44732534+FMS0102@users.noreply.github.com>
2022-10-21 00:58:00 -0300

    Fix AboutMe

--- frontend/src/components/AboutMe/index.tsx
+++ frontend/src/components/AboutMe/index.tsx
@@ -22,7 +22,7 @@ function AboutMe() {
                 <div className="details">
                     <p>
                         Sou programador iniciante e comecei os meus estudos há quatro anos,
-                        atualmente eu estou cursando análise e desenvolvimento de sistemas e procuro
+                        atualmente eu estou cursando análise e desenvolvimento de sistemas e pretendo
                         ingressar no mercado de TI.
                     </p>
 
