dd5c3d55ecb6e1928f0da2d4a635b07067a293c9
Felipe M. Simões <44732534+FMS0102@users.noreply.github.com>
2023-02-03 19:12:33 -0300

    Fiz AboutMe details

--- frontend/src/components/AboutMe/index.tsx
+++ frontend/src/components/AboutMe/index.tsx
@@ -26,14 +26,6 @@ function AboutMe() {
                         ingressar no mercado de TI.
                     </p>
 
-                    <p>
-                        Minha jornada profissional começou em 2010, quando obtive a oportunidade de
-                        entrar em uma empresa no ramo de construção civil, como servente de campo no setor de topografia e
-                        posteriormente no suprimentos, como almoxarife. Com o tempo, surgiu uma oportunidade de
-                        trabalhar como comprador, foi quando eu ingressei nessa área e trabalhei em diversas obras pelo Brasil.
-                        Atualmente estou trabalhando nessa função, em uma empresa que atua no segmento de saneamento no ES.
-                    </p>
-
                 </div>
             </div>
         </div>
